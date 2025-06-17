0488ee05d3895f20573f28fc45c31f5d06c29c62
Finn Terdal <finnterdal@Finns-MBP.nyc.rr.com>
2025-06-17 16:45:39 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log("Hello! If you see this, the script is working.");
+
+/*
+- [ ] Select the section with an id of container without using querySelector.
+- [ ] Select the section with an id of container using querySelector.
+- [ ] Select all of the list items with a class of "second".
+- [ ] Select a list item with a class of third, but only the list item inside of the ol tag.
+- [ ] Give the section with an id of container the text "Hello!".
+- [ ] Add the class main to the div with a class of footer.
+- [ ] Remove the class main on the div with a class of footer.
+- [ ] Create a new li element.
+- [ ] Give the li the text "four".
+- [ ] Append the li to the ul element.
+- [ ] Loop over all of the lis inside the ol tag and give them a background color of "green".
+- [ ] Remove the div with a class of footer.
+*/
+
+// Try rewriting this without using querySelector
+const header = document.querySelector("#container");
+console.log("header", header);
